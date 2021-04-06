32614202c303e1dac09a98aed8520972ff1e0bd0
PolyAtomicIon <180107265@stu.sdu.edu.kz>
2021-04-06 16:09:27 +0600

    works finally -> something

--- src/components/CalendarApp.vue
+++ src/components/CalendarApp.vue
@@ -1,7 +1,11 @@
 <template>
-  <v-app id="dayspan">
-    <ds-calendar-app :calendar="calendar"></ds-calendar-app>
-  </v-app>
+  <v-app id="dayspan" v-cloak> 
+    <ds-calendar
+      :calendar="calendar"
+    >
+      
+    </ds-calendar>
+  </v-app>  
 </template>
 
 <script>
@@ -10,15 +14,30 @@ import { Calendar } from 'dayspan';
 export default {
   name: 'app',
   data: () => ({
-    calendar: Calendar.months()
-  })
+    calendar: Calendar.days(),
+    // types: [
+    //   {id: 'D', label: 'day', shortcut: 'D', type: Units.DAY,   size: 1,  focus: 0.4999, repeat: true,  listTimes: true,  updateRows: true,  schedule: false },
+    //   {id: 'W', label: 'week', shortcut: 'W', type: Units.WEEK,  size: 1,  focus: 0.4999, repeat: true,  listTimes: true,  updateRows: true,  schedule: false },
+    // ]
+    // day: 
+  }),
+  mounted(){
+  }
 }
 </script>
 
 <style>
-body, html, #app, #dayspan {
-  font-family: Roboto, sans-serif;
-  width: 100%;
-  height: 100%;
-}
+  body, html, #app, #dayspan {
+    font-family: Roboto, sans-serif;
+    width: 100%;
+    height: 100%;
+  }
+
+  .ds-calendar{
+    min-height: 20% !important;
+  }
+
+  .v-application--wrap {
+    min-height: 50vh;
+  }
 </style>

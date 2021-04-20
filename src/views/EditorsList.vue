d6c9430809800cc8d251d7ee26f0998f5ff2bae1
PolyAtomicIon <180107265@stu.sdu.edu.kz>
2021-04-21 02:09:55 +0600

    editors list

--- src/views/EditorsList.vue
+++ src/views/EditorsList.vue
@@ -1,5 +1,7 @@
 <template>
+    <div>
         <v-card
+            max-height="265px"
         >
             <v-card-title class="white--text orange darken-4">
                 Editors List
@@ -25,7 +27,7 @@
             <v-virtual-scroll
                 :items="items"
                 :item-height="50"
-                height="300"
+                height="125"
             >
                 <template v-slot:default="{ item }">
                 <v-list-item>
@@ -62,6 +64,73 @@
                 </template>
             </v-virtual-scroll>
         </v-card>
+
+        <v-card
+            max-height="265px"
+            class="my-4"
+        >
+            <v-card-title class="white--text indigo darken-4">
+                Shared with
+        
+                <v-spacer></v-spacer>
+        
+                <v-btn
+                color="white"
+                class="text--primary"
+                fab
+                small
+                >
+                <v-icon>mdi-plus</v-icon>
+                </v-btn>
+            </v-card-title>
+        
+            <v-card-text class="pt-4">
+                You are allowed to give acces to edit your entriesatmost for 2 employees 
+            </v-card-text>
+        
+            <v-divider></v-divider>
+        
+            <v-virtual-scroll
+                :items="items"
+                :item-height="50"
+                min-height="125"
+            >
+                <template v-slot:default="{ item }">
+                <v-list-item>
+                    <v-list-item-avatar>
+                    <v-avatar
+                        :color="item.color"
+                        size="56"
+                        class="white--text"
+                    >
+                        {{ item.initials }}
+                    </v-avatar>
+                    </v-list-item-avatar>
+        
+                    <v-list-item-content>
+                    <v-list-item-title>{{ item.fullName }}</v-list-item-title>
+                    </v-list-item-content>
+        
+                    <v-list-item-action>
+                    <v-btn
+                        depressed
+                        small
+                    >
+                        View User
+        
+                        <v-icon
+                        color="orange darken-4"
+                        right
+                        >
+                        mdi-open-in-new
+                        </v-icon>
+                    </v-btn>
+                    </v-list-item-action>
+                </v-list-item>
+                </template>
+            </v-virtual-scroll>
+        </v-card>
+    </div>
 </template>
 
 <script>
